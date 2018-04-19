fc1766bf6ea73ce50310b87b785ea632e4f58341
Roman Bidnyy <romanbidnyy@romans-macbook-pro.local>
2018-04-19 22:15:40 +0100

    Initial Commit

---
+++ cargame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cargame
+//
+//  Created by Roman Bidnyy on 19/04/2018.
+//  Copyright © 2018 Roman Bidnyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
