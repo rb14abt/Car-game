777b32909f2d7acaf95a52bd00224d5cb18403aa
Roman Bidnyy <romanbidnyy@romans-macbook-pro.local>
2018-04-19 22:40:18 +0100

    added animation for background dragview player car added too

--- cargame/ViewController.swift
+++ cargame/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
-
+let W = UIScreen.main.bounds.width
+let H = UIScreen.main.bounds.height
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var imageArray: [UIImage]!
+        
+        imageArray = [UIImage(named: "road1.png")!,
+                     UIImage(named: "road2.png")!,
+                     UIImage(named: "road3.png")!,
+                     UIImage(named: "road4.png")!,
+                     UIImage(named: "road5.png")!,
+                     UIImage(named: "road6.png")!,
+                     UIImage(named: "road7.png")!,
+                     UIImage(named: "road8.png")!,
+                     UIImage(named: "road9.png")!,
+                     UIImage(named: "road10.png")!,
+                     UIImage(named: "road11.png")!,
+                     UIImage(named: "road12.png")!,
+                     UIImage(named: "road13.png")!,
+                     UIImage(named: "road14.png")!,
+                     UIImage(named: "road15.png")!,
+                     UIImage(named: "road16.png")!,
+                     UIImage(named: "road17.png")!,
+                     UIImage(named: "road18.png")!,
+                     UIImage(named: "road19.png")!,
+                     UIImage(named: "road20.png")!]
+        
+        
+     background.image = UIImage.animatedImage(with: imageArray, duration: 3)
+     self.background.frame = CGRect(x: 0, y: 0, width: W, height: H)
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
